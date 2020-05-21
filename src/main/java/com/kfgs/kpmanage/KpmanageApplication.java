be6cc87129b5746fa5fea71e855a1f9d61444098
lsy <923372823@qq.com>
2020-05-21 15:03:15 +0800

    kp管理

---
+++ src/main/java/com/kfgs/kpmanage/KpmanageApplication.java
@@ -0,0 +1,13 @@
+package com.kfgs.kpmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kfgs/kpmanage/KpmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfgs.kpmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
