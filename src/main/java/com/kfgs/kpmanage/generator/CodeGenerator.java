d71297deb9a15474f3ca33a4b4c0738683bdf2fb
lsy <923372823@qq.com>
2020-05-21 17:19:46 +0800

    generator

---
+++ src/main/java/com/kfgs/kpmanage/generator/CodeGenerator.java
@@ -0,0 +1,114 @@
+package com.kfgs.kpmanage.generator;
+
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    // xml 文件模板
+    private static final String XML_MAPPER_TEMPLATE_PATH = "generator/templates/mapper.xml";
+    // dao 文件模板
+    private static final String MAPPER_TEMPLATE_PATH = "generator/templates/mapper.java";
+    // entity 文件模板
+    private static final String ENTITY_TEMPLATE_PATH = "generator/templates/entity.java";
+    // service 文件模板
+    private static final String SERVICE_TEMPLATE_PATH = "generator/templates/service.java";
+    // serviceImpl 文件模板
+    private static final String SERVICE_IMPL_TEMPLATE_PATH = "generator/templates/serviceImpl.java";
+    // controller 文件模板
+    private static final String CONTROLLER_TEMPLATE_PATH = "generator/templates/controller.java";
+
+    public static void main(String[] args){
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        //输出文件路径
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("lsy");
+        gc.setOpen(false);
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(true);
+        //生成的service接口名字首字母是否为I，这样设置就没有I
+        gc.setServiceName("%sService");
+        //实体属性 Swagger2 注解
+        gc.setSwagger2(true);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:oracle:thin:@127.0.0.1:1521:orcl");
+        dsc.setDriverName("oracle.jdbc.driver.OracleDriver");
+        dsc.setUsername("zljg");
+        dsc.setPassword("zljg123456");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("");
+        pc.setParent("com.kfgs.kpmanage");
+        /*pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setMapper("mapper");
+        pc.setEntity("entity");*/
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 配置自定义代码模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(XML_MAPPER_TEMPLATE_PATH);
+        templateConfig.setMapper(MAPPER_TEMPLATE_PATH);
+        templateConfig.setEntity(ENTITY_TEMPLATE_PATH);
+        templateConfig.setService(SERVICE_TEMPLATE_PATH);
+        templateConfig.setServiceImpl(SERVICE_IMPL_TEMPLATE_PATH);
+        templateConfig.setController(CONTROLLER_TEMPLATE_PATH);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setInclude("表名");
+        //strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        //strategy.setTablePrefix(packageConfig.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+    private static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(("请输入" + tip + "："));
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) return ipt;
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+}
