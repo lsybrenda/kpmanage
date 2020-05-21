24a6cfa7e54dc8e08c00a254fb0514383c87fe61
lsy <923372823@qq.com>
2020-05-21 15:32:08 +0800

    加入swagger

---
+++ src/main/java/com/kfgs/kpmanage/common/config/Swagger2Config.java
@@ -0,0 +1,53 @@
+package com.kfgs.kpmanage.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        List<Parameter> pars = new ArrayList<>();
+
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        ticketPar.name("token").description("token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+        pars.add(ticketPar.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("kpmanage RESTful API")
+                .description("考评管理后台功能测试")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+
+}
