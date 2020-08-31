6142b52388d82ae57ad795db656425c662475727
lsy <923372823@qq.com>
2020-08-31 15:38:36 +0800

    统一错误码

--- src/main/java/com/kfgs/kpmanage/common/utils/ReadWord.java
+++ src/main/java/com/kfgs/kpmanage/common/utils/ReadWord.java
@@ -1,7 +1,12 @@
 package com.kfgs.kpmanage.common.utils;
 
+import com.kfgs.kpmanage.entity.GradeExamination;
 import org.apache.poi.POIXMLDocument;
 import org.apache.poi.POIXMLTextExtractor;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
@@ -14,6 +19,8 @@ import org.apache.poi.hwpf.extractor.WordExtractor;
 import java.io.*;
 import java.text.DecimalFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReadWord {
 
@@ -21,19 +28,20 @@ public class ReadWord {
     static int number=0;
     public ReadWord(String filePath){
 
+        String candidate = filePath.substring(0,filePath.indexOf("."));
         String content = "";
         try {
             if (filePath.endsWith(".doc")){
                 InputStream inputStream = new FileInputStream(new File(filePath));
                 WordExtractor extractor = new WordExtractor(inputStream);
                 content = extractor.getText();
-                getContent(content);
+                getContent(candidate,content);
                 extractor.close();
             }else if (filePath.endsWith("docx")){
                 OPCPackage opcPackage = POIXMLDocument.openPackage(filePath);
                 POIXMLTextExtractor extractor = new XWPFWordExtractor(opcPackage);
                 content = extractor.getText();
-                getContent(content);
+                getContent(candidate,content);
                 extractor.close();
             }else {
                 return;
@@ -49,7 +57,9 @@ public class ReadWord {
         }
     }
 
-    private void getContent(String text){
+    private void getContent(String candidate, String text){
+        List<GradeExamination> list = new ArrayList<>();
+        GradeExamination gradeExamination = new GradeExamination();
         String[] strs = text.split("\n");
         String name = "";
         ArrayList<String> yjmbList = new ArrayList<String>();
@@ -99,13 +109,45 @@ public class ReadWord {
                 sameworkid = ss.substring(1,7);
                 value = value.substring(0,value.length()-7);
             }
+            Pattern pattern = Pattern.compile("[0-9]*");
+            Matcher isNum = pattern.matcher(value.charAt(0)+"");
+            if (!isNum.matches()){
+                String thisNum = Integer.toString(i+1)+".";
+                value = thisNum.concat(value);
+            }
             String num = decimalFormat.format(i+1);
             //获取当前年份
             Calendar cal = Calendar.getInstance();
             String year = Integer.toString(cal.get(Calendar.YEAR)) + "A";
             String exa_id = year + userID + num;
-            System.out.println(exa_id + "    "+ value + "  "+ userID + sameworkid );
+            System.out.println(exa_id + "    "+ value + "   "+ userID + sameworkid );
+
+            //创建Excel对象
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            //创建工作表单
+            HSSFSheet sheet = workbook.createSheet("个人考核内容");
+            //创建HSSFRow对象
+            HSSFRow row = sheet.createRow(0);
+            //创建单元格，设置表头
+            HSSFCell cell = row.createCell(0);
+            cell.setCellValue("考核条目id");
+            cell = row.createCell(1);
+            cell.setCellValue("考核内容");
+            cell = row.createCell(2);
+            cell.setCellValue("所属员工id");
+            cell = row.createCell(3);
+            cell.setCellValue("相同条目标记");
+
+            writeToExcel(candidate,exa_id,value,userID,sameworkid);
         }
+    }
+
+    /**
+     * 将解析的Word内容写进Excel
+     */
+    public void writeToExcel(String candidate,String exa_id,String value,String userID,String sameworkid){
+
+
     }
 
     /*public static void main(String[] args) throws FileNotFoundException {
--- src/main/java/com/kfgs/kpmanage/entity/response/CommonCode.java
+++ src/main/java/com/kfgs/kpmanage/entity/response/CommonCode.java
@@ -6,9 +6,33 @@ import lombok.ToString;
 public enum  CommonCode implements ResultCode {
 
     SUCCESS(true,20000,"操作成功"),
-    FAIL(false,11111,"操作失败"),
-    SERVER_ERROR(false,10002,"权限不足，无法操作"),
-    INVALID_PARAM(false,10003,"非法参数");
+    FAIL(false,99999,"操作失败"),
+
+    /**
+     * 业务错误:10001~19999
+     */
+    NO_PERMISSION(false,10001,"权限不足，无法操作"),
+
+    /**
+     * 参数错误:30001~39999
+     */
+    PARAM_NOT_VALID(false,30001,"非法参数"),
+    PARAM_IS_BLANK(false,30002,"参数为空"),
+    PARAM_TYPE_ERROR(false,30003,"参数类型错误"),
+    PARAM_NOT_COMPLETE(false,30004,"参数缺失"),
+
+    /**
+     * 用户错误
+     */
+    USER_NOT_LOGIN(false,20001,"用户未登录"),
+    USER_ACCOUNT_EXPIRED(false,20002,"账号已过期"),
+    USER_CREDENTIALS_ERROR(false,20003,"密码错误"),
+    USER_CREDENTIALS_EXPORED(false,20004,"密码过期"),
+    USER_ACCOUNT_DISABLE(false,20005,"账号不可用"),
+    USER_ACCOUNT_LOCKED(false,20006,"账号被锁定"),
+    USER_ACCOUNT_NOT_EXIST(false,20007,"账号不存在"),
+    USER_ACCOUNT_ALREADY_EXIST(false,20008,"账号已存在"),
+    USER_ACCOUNT_USE_BY_OTHERS(false,20009,"账号下线");
 
     //操作是否成功
     boolean success;
