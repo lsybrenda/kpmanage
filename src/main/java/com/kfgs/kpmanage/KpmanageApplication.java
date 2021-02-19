a67eb9e5d168f6c731e582468b0c951308128689
lsy <923372823@qq.com>
2021-02-19 10:48:12 +0800

    登录功能增加日志

--- src/main/java/com/kfgs/kpmanage/KpmanageApplication.java
+++ src/main/java/com/kfgs/kpmanage/KpmanageApplication.java
@@ -1,18 +1,17 @@
 package com.kfgs.kpmanage;
 
+import org.apache.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.logging.Logger;
 
 @MapperScan("com.kfgs.kpmanage.mapper")
 @SpringBootApplication
 @EnableScheduling
 public class KpmanageApplication {
 
+    private static Logger logger = Logger.getLogger(KpmanageApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(KpmanageApplication.class, args);
--- src/main/java/com/kfgs/kpmanage/controller/LoginController.java
+++ src/main/java/com/kfgs/kpmanage/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.kfgs.kpmanage.controller;
 
+import com.kfgs.kpmanage.KpmanageApplication;
 import com.kfgs.kpmanage.entity.GradeIdentity;
 import com.kfgs.kpmanage.entity.GradeUserinfo;
 import com.kfgs.kpmanage.entity.response.CommonCode;
@@ -8,7 +9,7 @@ import com.kfgs.kpmanage.entity.response.QueryResult;
 import com.kfgs.kpmanage.mapper.GradeIdentityMapper;
 import com.kfgs.kpmanage.mapper.GradeUserinfoMapper;
 import com.kfgs.kpmanage.service.GradeIdentityService;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +23,12 @@ public class LoginController {
     @Autowired
     GradeIdentityService gradeIdentityService;
 
+    private static Logger logger = Logger.getLogger(KpmanageApplication.class);
+
     //配置拦截字符串
     @PostMapping("/login")
     public QueryResponseResult login(String userid,String password){
+        logger.info("用户"+userid+"开始登录...");
         System.out.println(userid + password);
         HashMap map = new HashMap();
         /*String userid = gradeIdentity.getUserid();
@@ -33,6 +37,7 @@ public class LoginController {
         //判断用户名是否存在
         int sum = gradeIdentityService.userIDJudge(userid);
         if (sum == 0){
+            logger.error(userid+"账号不存在");
             return new QueryResponseResult(CommonCode.USER_ACCOUNT_NOT_EXIST,null);
         }
         String loginpass = gradeIdentityService.getPasswordByID(userid);
@@ -41,8 +46,10 @@ public class LoginController {
             map.put("userid",userid);
             map.put("password",loginpass);
             queryResult.setMap(map);
+            logger.info(userid+"登录成功");
             return new QueryResponseResult(CommonCode.SUCCESS,queryResult);
         }else {
+            logger.error(userid+"登录失败");
             return new QueryResponseResult(CommonCode.USER_CREDENTIALS_ERROR,null);
         }
     }
