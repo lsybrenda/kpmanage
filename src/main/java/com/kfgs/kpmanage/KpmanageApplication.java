3f4ccc1d67600f7d78bb870e9f1aae2a603280d1
lsy <923372823@qq.com>
2021-02-19 09:55:24 +0800

    加入log日志

--- src/main/java/com/kfgs/kpmanage/KpmanageApplication.java
+++ src/main/java/com/kfgs/kpmanage/KpmanageApplication.java
@@ -1,12 +1,18 @@
 package com.kfgs.kpmanage;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.logging.Logger;
 
 @MapperScan("com.kfgs.kpmanage.mapper")
 @SpringBootApplication
+@EnableScheduling
 public class KpmanageApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(KpmanageApplication.class, args);
